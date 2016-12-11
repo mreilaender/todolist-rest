c745d0b8098502b8d1646fc24803d469a62ac21a
mreilaender <manuel.reilaender@gmail.com>
2016-12-11 20:29:47 +0100

    add webmvc

--- src/main/java/com/mreilaender/todolist/controller/VersionController.java
+++ src/main/java/com/mreilaender/todolist/controller/VersionController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author manuel
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @CrossOrigin(origins = "https://mytodolist-react.herokuapp.com/")
 @RestController
+@EnableWebMvc
 @RequestMapping("/version")
 public class VersionController {
     private static final String VERSION = "0.1.1";
